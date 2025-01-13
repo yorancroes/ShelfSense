ea98c4959276fe0501bc97412400026cdf546129
StijnBuedts <60989927+StijnBuedts@users.noreply.github.com>
2025-01-13 16:24:55 +0100

    register/login veranderd + menu aangemaakt

--- app/frontend/index.py
+++ app/frontend/index.py
@@ -4,8 +4,6 @@ from PyQt6.uic import loadUi #is voor QT Designer als mag gebruiken
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 import os
-from backend.user import User
-
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,15 +11,28 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi(os.path.join(BASE_DIR, "UI/main.ui"), self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.loginButton.clicked.connect(self.clicked)
-
+        self.registerButton.clicked.connect(self.clickedRegi)
+        self.closeButton.clicked.connect(self.closing)
+        self.miniButton.clicked.connect(self.mini)
 
     def clicked(self):
         self.win = LoginWindow()
         self.win.show()
         self.close()
 
+    def clickedRegi(self):
+        self.win = RegisterWindow()
+        self.win.show()
+        self.close()
 
+    def closing(self):
+        self.close()
+
+    def mini(self):
+        self.showMinimized()
 
 class LoginWindow(QMainWindow):
     def __init__(self):
@@ -29,17 +40,58 @@ class LoginWindow(QMainWindow):
         uic.loadUi(os.path.join(BASE_DIR, "UI/login.ui"), self)
         self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
-        self.loginButton.clicked.connect(self.clicked)
-
+        self.terugButton.clicked.connect(self.clicked)
+        self.closeButton.clicked.connect(self.closing)
+        self.miniButton.clicked.connect(self.mini)
+        self.loginButton.clicked.connect(self.login)
 
     def clicked(self):
-        username = self.lineEdit.text()
-        password = self.lineEdit_2.text()
-        gebruiker = User(username, password)
-        gebruiker.insert_user()
+        self.win = RegisterWindow()
+        self.win.show()
+        self.close()
 
-        if gebruiker.verify_password():
-            print(password)
+    def closing(self):
+        self.close()
 
+    def mini(self):
+        self.showMinimized()
 
+    def login(self):
+        self.win = MenuWindow()
+        self.win.show()
+        self.close()
 
+class RegisterWindow(QMainWindow):
+    def __init__(self):
+        super(RegisterWindow, self).__init__()
+        uic.loadUi(os.path.join(BASE_DIR, "UI/register.ui"), self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.terugButton.clicked.connect(self.clicked)
+        self.closeButton.clicked.connect(self.closing)
+        self.miniButton.clicked.connect(self.mini)
+
+    def clicked(self):
+        self.win = LoginWindow()
+        self.win.show()
+        self.close()
+
+    def closing(self):
+        self.close()
+
+    def mini(self):
+        self.showMinimized()
+
+class MenuWindow(QMainWindow):
+    def __init__(self):
+        super(MenuWindow, self).__init__()
+        uic.loadUi(os.path.join(BASE_DIR, "UI/menu.ui"), self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    #main_window.showMaximized();
+    main_window.show()  # Toon het hoofdvenster
+    sys.exit(app.exec())
