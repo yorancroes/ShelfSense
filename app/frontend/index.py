1e46dadffd44cf1711c398f52dfb64b9165bdfc5
1flyti <yoran.croes24@gmail.com>
2025-01-14 17:51:14 +0100

    made the index merge correctly only logs in when right password and username combo is given

--- app/frontend/index.py
+++ app/frontend/index.py
@@ -6,15 +6,18 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 import os
 from backend.user import User
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi(os.path.join(BASE_DIR, "UI/main.ui"), self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.loginButton.clicked.connect(self.clicked)
-
+        self.registerButton.clicked.connect(self.clickedRegi)
+        self.closeButton.clicked.connect(self.closing)
+        self.miniButton.clicked.connect(self.mini)
 
     def clicked(self):
         self.win = LoginWindow()
@@ -22,6 +25,16 @@ class MainWindow(QMainWindow):
         self.close()
 
 
+    def clickedRegi(self):
+        self.win = RegisterWindow()
+        self.win.show()
+        self.close()
+
+    def closing(self):
+        self.close()
+
+    def mini(self):
+        self.showMinimized()
 
 class LoginWindow(QMainWindow):
     def __init__(self):
@@ -29,8 +42,48 @@ class LoginWindow(QMainWindow):
         uic.loadUi(os.path.join(BASE_DIR, "UI/login.ui"), self)
         self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
-        self.loginButton.clicked.connect(self.clicked)
+        self.terugButton.clicked.connect(self.clicked)
+        self.closeButton.clicked.connect(self.closing)
+        self.miniButton.clicked.connect(self.mini)
+        self.loginButton.clicked.connect(self.login)
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        gebruiker = User(username, password)
+        gebruiker.insert_user()
 
+    def clicked(self):
+        self.win = RegisterWindow()
+        self.win.show()
+        self.close()
+
+    def closing(self):
+        self.close()
+
+    def mini(self):
+        self.showMinimized()
+
+    def login(self):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        gebruiker = User(username, password)
+
+
+        if gebruiker.verify_password(password):
+            self.win = MenuWindow()
+            self.win.show()
+            self.close()
+
+
+class RegisterWindow(QMainWindow):
+    def __init__(self):
+        super(RegisterWindow, self).__init__()
+        uic.loadUi(os.path.join(BASE_DIR, "UI/register.ui"), self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.loginButton.clicked.connect(self.clicked)
+        self.terugButton.clicked.connect(self.login)
+        self.closeButton.clicked.connect(self.closing)
+        self.miniButton.clicked.connect(self.mini)
 
     def clicked(self):
         username = self.lineEdit.text()
@@ -38,8 +91,31 @@ class LoginWindow(QMainWindow):
         gebruiker = User(username, password)
         gebruiker.insert_user()
 
-        if gebruiker.verify_password():
-            print(password)
+        self.win = LoginWindow()
+        self.win.show()
+        self.close()
 
+    def login(self):
+        self.win = LoginWindow()
+        self.win.show()
+        self.close()
 
+    def closing(self):
+        self.close()
 
+    def mini(self):
+        self.showMinimized()
+
+class MenuWindow(QMainWindow):
+    def __init__(self):
+        super(MenuWindow, self).__init__()
+        uic.loadUi(os.path.join(BASE_DIR, "UI/menu.ui"), self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    #main_window.showMaximized();
+    main_window.show()  # Toon het hoofdvenster
+    sys.exit(app.exec())
