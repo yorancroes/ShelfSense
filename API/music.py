b8678d296e1ceea7502f13b382fcb21459401bc3
komuts <komutsalih61@icloud.com>
2025-02-01 21:33:13 +0100

    searchMusic functie aangepast zodat alles in een lijst van albums wordt geplaatst die de functie dan teruggeeft als output

--- API/music.py
+++ API/music.py
@@ -18,21 +18,22 @@ def searchMusic(album_name):
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
         data = response.json()
-        print(data)
-    else:
-        print(f'Error: {response.status_code}')
-    albums = data['results']['albummatches']['album']
-    album_name = next((album['name'] for album in albums if album['name'] == album_name), None)
-    artist_name = next((album['artist'] for album in albums if album['name'] == album_name), None)
-    image_url = next(
-        (img['#text'] for album in albums if album['name'] == album_name for img in album['image'] if
-         img['size'] == "large"),
-        None
-    )
-    print(f"{album_name} - {artist_name} - {image_url}")
+        albums = data.get("results", {}).get("albummatches", {}).get("album", [])
+        albums_filtered = [
+            {
+                "name": album.get("title"),
+                "artist": album.get("artist"),
+                "image": {
+                    "large": next((image.get("#text") for image in album.get("image", []) if image.get("size") == "large"), None)
+                }
+            }
+            for album in albums
+        ]
+        return albums_filtered
 
-searchMusic('Thriller')
-
+albums = searchMusic("Thriller")
+for album in albums:
+    print(album)
 
 #response structuur voor referentie
 #name: "AlBUM_Name" | artist: "ARTIST_Name" | image: [...{'text': "URL_IMAGE", 'size' : "medium"}...]
