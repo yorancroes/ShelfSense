499defb739d02b945c209b482a9f1a5f323b222d
komuts <komutsalih61@icloud.com>
2025-02-09 20:08:10 +0100

    API van games sneller gemaakt (11seconden search naar 6 seconden) met behulp van concurrent.futures library

--- API/games.py
+++ API/games.py
@@ -1,18 +1,16 @@
 import os
 import requests
+import concurrent.futures
 from dotenv import load_dotenv
 
 load_dotenv()
 MY_API_KEY = os.getenv("GAMES_API_KEY")
-
 
 BASE_URL = "https://www.giantbomb.com/api"
 HEADERS = {"User-Agent": "YourAppName"}
 
 
 def get_game_details(game_id):
-    #we moeten bij deze api een aparte game details call aanmaken om de developer en publisher te krijgen.
-    #dit is een vereiste omdat tijdens mijn testen de api geen developer en publisher teruggeeft als we geen call doen op een id voor de details.
     params = {
         "api_key": MY_API_KEY,
         "format": "json"
@@ -31,13 +29,19 @@ def search_games(game_name):
         "resources": "game"
     }
     response = requests.get(f"{BASE_URL}/search/", params=params, headers=HEADERS)
+
     if response.status_code == 200:
         data = response.json()
         games = []
-        for item in data.get("results", []): #voor elke entry doen we een dubbele call voor de developer en publisher info te krijgen
-            game_id = item.get("id")
-            game_details = get_game_details(game_id) if game_id else {}
-            game_info = {
+        game_ids = {item["id"]: item for item in data.get("results", []) if "id" in item}
+
+        # Parallel details ophalen
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            game_details_map = {game_id: executor.submit(get_game_details, game_id) for game_id in game_ids}
+
+        for game_id, item in game_ids.items():
+            game_details = game_details_map[game_id].result()  # Resultaat ophalen van toekomstige taak
+            games.append({
                 "name": item.get("name"),
                 "platforms": [platform["name"] for platform in item.get("platforms", [])] if item.get(
                     "platforms") else [],
@@ -47,7 +51,6 @@ def search_games(game_name):
                 "publisher": [pub["name"] for pub in game_details.get("publishers", [])] if game_details.get(
                     "publishers") else ["Unknown"],
                 "cover_image": item.get("image", {}).get("original_url")
-            }
-            games.append(game_info)
-        return games #we combineren beide info van beide calls en steken ze in de games list
+            })
 
+        return games
